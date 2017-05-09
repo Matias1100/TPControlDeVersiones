a0ae92bec4e88b294efe1b3f2b78364950ae3de0
Matias1100 <matiasfmadrid@hotmail.com>
2017-05-09 00:42:31 -0300

    Add files via upload

---
+++ TP1Punto3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1.punto.pkg3;
+
+/**
+ *
+ * @author Josemi
+ */
+public class TP1Punto3 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Aleatorio x=new Aleatorio();
+        System.out.println(x.toString());
+        x.analizarValores();
+        System.out.println(x.toString());       
+        
+    }
+    
+}
